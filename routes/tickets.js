19eb04f31c66f6cb7a42e789f08f2892e3ff7c7e
Guillaume <ronnel.guillaume@proton.me>
2023-04-11 14:57:08 +0200

    UPDATE achat ticket / DELETE supprime ticket panier

--- routes/tickets.js
+++ routes/tickets.js
@@ -3,23 +3,36 @@ var router = express.Router();
 const Ticket = require('./../models/tickets')
 const moment = require('moment')
 
-
+// Recupere les tickets dans le panier
 router.get('/cart', (req, res) => {
     Ticket.find({isPayed: false}).populate('trip').then(tick => res.json(tick))
 })
 
+// Status du ticket acheté
+router.post('/cart/:tickId', (req, res) => {
+    Ticket.updateOne({ _id: req.params.tickId}, { isPayed: true}).then(tick => res.json(tick))
+})
+
+// Supprime le ticket du panier
+router.delete('/cart/:tickId', (req, res) => {
+    Ticket.deleteOne({ _id: req.params.tickId}).then(tick => res.json(tick))
+})
+
+// Recupere les tickets achetés
 router.get('/booked', (req, res) => {
     Ticket.find({isPayed: true}).populate('trip').then(tick => res.json(tick))
 })
 
-
+// Ajoute le ticket au panier
 router.post('/:tripId', (req, res) => {
     const newTicket = new Ticket({
-        isPayed: true,
+        isPayed: false,
         trip: { _id : req.params.tripId }
     })
     
     newTicket.save().then(() => res.json(newTicket))
 })
+
+
 
 module.exports = router
